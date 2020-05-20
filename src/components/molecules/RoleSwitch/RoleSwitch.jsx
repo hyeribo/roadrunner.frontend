57a859c2cd9908d17c63de96921b33214c8dd056
hyeribo <harmfuli59@gmail.com>
2020-05-20 23:12:51 +0900

    모드 수정 컴포넌트 프롭추가

--- src/components/molecules/RoleSwitch/RoleSwitch.jsx
+++ src/components/molecules/RoleSwitch/RoleSwitch.jsx
@@ -6,11 +6,18 @@ import React from "react";
 import { Radio } from "antd";
 
 const RoleSwitch = (props) => {
+  const { onChange, value, ...rest } = props;
   return (
-    <div className="rr-role-switch" {...props}>
-      <Radio.Group defaultValue="r" size="large" buttonStyle="solid">
-        <Radio.Button value="r">Runner</Radio.Button>
-        <Radio.Button value="s">Shopper</Radio.Button>
+    <div className="rr-role-switch" {...rest}>
+      <Radio.Group
+        defaultValue="r"
+        size="large"
+        buttonStyle="solid"
+        onChange={onChange}
+        value={value}
+      >
+        <Radio.Button value="runner">Runner</Radio.Button>
+        <Radio.Button value="shopper">Shopper</Radio.Button>
       </Radio.Group>
     </div>
   );
