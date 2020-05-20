6d9216e8a7b93ceea25afd89530f5e619cedd603
hyeribo <harmfuli59@gmail.com>
2020-05-20 23:09:54 +0900

    공통 레이아웃

---
+++ src/components/organisms/Bottoms/CommonBottom.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const BottomTab = ({ tabName }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="rr-common-bottom">
+      <div className="global-content-wrapper">
+        <div className="bottom-content">asd</div>
+      </div>
+    </div>
+  );
+};
+
+export default BottomTab;
--- src/components/templates/Layouts/CommonLayout.jsx
+++ src/components/templates/Layouts/CommonLayout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import CommonHeader from "@organisms/Headers/CommonHeader";
+import CommonBottom from "@organisms/Bottoms/CommonBottom";
 
 const CommonLayout = (props) => {
   return (
@@ -11,6 +12,9 @@ const CommonLayout = (props) => {
           <div className="rr-common-layout-content">{props.children}</div>
         </div>
       </div>
+      {props.bottomButtons && props.bottomButtons.length && (
+        <CommonBottom buttons={props.bottomButtons} />
+      )}
     </div>
   );
 };
