0efccf218c2b3f694333a7780c8eca1e16e6f26f
mlee990125 <mlee0125@gmail.com>
2020-05-31 20:25:20 +0900

    Added request detail page

--- src/components/pages/Request/RequestDetail.jsx
+++ src/components/pages/Request/RequestDetail.jsx
@@ -2,12 +2,35 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import CommonLayout from "@templates/Layouts/CommonLayout";
+import UserInfo from "@organisms/UserInfo/UserInfo";
+
 
 const RequestDetail = () => {
+  const onClickButton = () =>{
+    console.log("hello");
+  }
+  const buttonProps = {
+    text: "회원가입",
+    onClick: onClickButton,
+    color: "disabled",
+  }
   return (
-    <CommonLayout>
-      <div>RequestDetail</div>
+    <CommonLayout 
+    pageName='hello'
+    showMenuBotton={false}
+    backgroundColor="#ffffff"
+    showBottom
+    buttonProps={buttonProps}
+    >
+      <div id="rr-request-detail-page" className="global-content-container">
+       <UserInfo type="runner" />
+
+      </div>
+      <div> 
+
+      </div>
     </CommonLayout>
+  
   );
 };
 
