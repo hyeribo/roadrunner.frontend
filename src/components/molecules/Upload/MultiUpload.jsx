de6c95c9bb6a263df2de875c808a2c1d464f5d2c
hyeribo <harmfuli59@gmail.com>
2020-06-16 00:15:03 +0900

    이미지 멀티업로드 추가

---
+++ src/components/molecules/Upload/MultiUpload.jsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Upload, message, Spin } from "antd";
+import { PlusOutlined, MinusOutlined } from "@ant-design/icons";
+
+import defaultImg from "@assets/images/img-default.png";
+
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+}
+
+const MultiUpload = (props) => {
+  const [images, setImages] = useState([]);
+  const { name, onChange, ...rest } = props;
+
+  const handleChange = (info) => {
+    if (info.file.status === "done") {
+      getBase64(info.file.originFileObj, (imageData) => {
+        setImages(
+          images.concat({
+            data: imageData,
+            path: info.file.response.data.files[0].path,
+          })
+        );
+      });
+    }
+  };
+
+  // 이미지 삭제
+  const handleRemoveImage = (index) => {
+    const removedImages = [...images];
+    removedImages.splice(index, 1);
+    setImages([...removedImages]);
+  };
+
+  useEffect(() => {
+    if (onChange) {
+      const paths = images.map((img) => img.path);
+      onChange(paths);
+    }
+  }, [images]);
+
+  return (
+    <div className="rr-multi-upload" {...rest}>
+      {!images.length ? (
+        <div className="image-wrapper">
+          <img src={defaultImg} />
+        </div>
+      ) : (
+        images.map((image, i) => (
+          <div className="image-wrapper">
+            <img key={i} src={image.data} />
+            <div className="image-remove">
+              <MinusOutlined onClick={() => handleRemoveImage(i)} />
+            </div>
+          </div>
+        ))
+      )}
+      <Upload
+        name={name}
+        showUploadList={false}
+        action={`${process.env.REACT_APP_API_BASE_URL}/upload/shopperOrderImages`}
+        beforeUpload={beforeUpload}
+        onChange={handleChange}
+      >
+        <div className="upload-button">
+          <PlusOutlined />
+        </div>
+      </Upload>
+    </div>
+  );
+};
+
+export default MultiUpload;
